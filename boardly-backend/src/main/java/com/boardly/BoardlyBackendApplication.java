4ca7c196f0c86b95ad5b393ee5e306c99511b53e
TheRealQi <therealqi@hotmail.com>
2026-01-29 14:28:32 +0200

    first commit

---
+++ boardly-backend/src/main/java/com/boardly/BoardlyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.boardly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardlyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardlyBackendApplication.class, args);
+    }
+
+}
